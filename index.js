76450348ead8d5a2b736e8215393def31f2a6b7b
romizaki <rzferdiyanto@gmail.com>
2021-04-14 20:50:08 +0700

    done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function print(numStart, numFinish) {
 // console.log(print(1,20));
 
 function findDoubleFirst(str) {
-  str = str.toLowerCase()
+  str = str.toUpperCase()
   for (let i = 0; i < str.length; i++) {
     if (str.indexOf(str.charAt(i)) !== str.lastIndexOf(str.charAt(i))) {
       return str.charAt(i)
@@ -27,3 +27,24 @@ function findDoubleFirst(str) {
 
 console.log(findDoubleFirst('ABBA'));
 console.log(findDoubleFirst('ABBC'));
+
+let arr1 = [1, 3, 13, 12, 2, 5]
+let arr2 = [1, 3, 7, 8, 2, 9]
+
+function sortArr(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] > array[j]) {
+        let temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+      }
+    }
+  }
+  array.unshift(array[array.length-1])
+  array.pop()
+  return array
+}
+
+console.log(sortArr(arr1))
+console.log(sortArr(arr2))
